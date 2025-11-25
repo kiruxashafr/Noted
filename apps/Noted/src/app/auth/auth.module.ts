b8b6998c1a2a147b34bfc50f79afe56a71c6094a
kiruxashafr <kirshafranov@gmail.com>
2025-11-25 22:12:31 +0300

    jwt

--- apps/Noted/src/app/auth/auth.module.ts
+++ apps/Noted/src/app/auth/auth.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { getJwtConfig } from './config/jwt.config';
+import { ConfigService } from "@nestjs/config";
+
 
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: getJwtConfig,
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
--- apps/Noted/src/app/auth/auth.service.ts
+++ apps/Noted/src/app/auth/auth.service.ts
@@ -1,11 +1,32 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { RegisterRequest } from './dto/register.dto';
+import { ConfigService } from '@nestjs/config';
 import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { StringValue } from 'jws';
+
+
+
+
+
+
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly prisma: PrismaService) {}
+    private readonly JWT_SECRET:string;
+    private readonly JWT_ACCESS_TOKEN_TTL:string;
+    private readonly JWT_REFRESH_TOKEN_TTL:string;
+
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly configService: ConfigService,
+        private readonly jwtService: JwtService,
+    ) {
+        this.JWT_SECRET = this.configService.getOrThrow<string>('JWT_SECRET');
+        this.JWT_ACCESS_TOKEN_TTL = this.configService.getOrThrow<string>('JWT_ACCESS_TOKEN_TTL');
+        this.JWT_REFRESH_TOKEN_TTL = this.configService.getOrThrow<string>('JWT_REFRESH_TOKEN_TTL');
+    }
 
     async register(dto: RegisterRequest) {
         const { name, email, password } = dto;
@@ -26,6 +47,19 @@ export class AuthService {
             },
         });
 
-        return user;
+        return this.generateTokens(user.id);
+        
 }
-}
+private generateTokens(userId: string) {
+    const payload = { sub: userId };
+
+    const accessToken = this.jwtService.sign(payload, {
+        expiresIn: this.JWT_ACCESS_TOKEN_TTL as StringValue, // импортируй из jws
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+        expiresIn: this.JWT_REFRESH_TOKEN_TTL as StringValue,
+    });
+
+    return { accessToken, refreshToken };
+}}
