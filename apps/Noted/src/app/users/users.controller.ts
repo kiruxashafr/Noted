1be35d23284329017d797892883578bf0490c50a
kiruxashafr <kirshafranov@gmail.com>
2025-11-25 19:16:41 +0300

    clean project

--- apps/Noted/src/app/users/users.controller.ts
+++ apps/Noted/src/app/users/users.controller.ts
@@ -1,8 +1,6 @@
 import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/users.dto';
-import { UsersPipe } from '../conception/users/users.pipe';
-import { UsersGuard } from '../conception/users/users.guard';
+
 
 @Controller('users')
 export class UsersController {
@@ -13,16 +11,4 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
-  @Get('quest')
-  @UseGuards(UsersGuard)
-  findDataForQuest(@Query('pageNumber', UsersPipe) pageNumber?: number) {
-    // console.log(pageNumber);
-
-    return this.usersService.findDataForQuest();
-  }
-
-  @Post()
-  create(@Body() dto: CreateUserDto) {
-    return this.usersService.create(dto);
-  }
 }
