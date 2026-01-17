f479ca252983c56ea586cc727bf105e813320b3b
kiruxashafr <kirshafranov@gmail.com>
2026-01-17 10:26:03 +0300

    fix: delete convert pipe, edit validation pipe

--- apps/Noted/src/app/pipes/image-validation.pipe.ts
+++ apps/Noted/src/app/pipes/image-validation.pipe.ts
@@ -4,15 +4,21 @@ import { ErrorCodes } from "@noted/common/errors/error-codes.const";
 
 @Injectable()
 export class ImageValidationPipe implements PipeTransform {
-  private readonly allowedMimeTypes = [
-    "image/jpeg",
-    "image/png",
-    "image/jpg",
-    "image/heic",
-    "image/heif",
-    "image/heic-sequence",
-    "image/heif-sequence",
-  ];
+private readonly allowedMimeTypes = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+  "image/heic",
+  "image/heif",
+  "image/heic-sequence",
+  "image/heif-sequence",
+  "image/tiff",
+  "image/avif",
+  "image/jp2",
+  "image/jpx",
+  "image/jpm",
+];
 
   transform(file: Express.Multer.File) {
     if (!file) {
