51dacd139da4d2691b12c69846c351e74ba700a5
kiruxashafr <kirshafranov@gmail.com>
2026-01-18 23:22:06 +0300

    test: user service delete user test

--- apps/Noted/src/app/users/users.service.spec.ts
+++ apps/Noted/src/app/users/users.service.spec.ts
@@ -3,24 +3,33 @@ jest.mock("argon2", () => ({
   hash: jest.fn(),
 }));
 
-jest.mock("@noted/common/utils/to-dto", () => ({
-  toDto: jest.fn(),
-}));
-
 import { PrismaService } from "../prisma/prisma.service";
 import { FilesService } from "../files/files.service";
 import { PhotoQueueService } from "../photo-queue/photo-queue.service";
 import { UserPlan } from "generated/prisma/enums";
 import { UsersService } from "../users/users.service";
 import { Test, TestingModule } from "@nestjs/testing";
+import { UpdateUserDto } from "./dto/user-update.dto";
+import { toDto } from "@noted/common/utils/to-dto";
+import { ReadUserDto } from "./dto/read-user.dto";
+import * as argon2 from "argon2";
+import { ApiException } from "@noted/common/errors/api-exception";
+import { ErrorCodes } from "@noted/common/errors/error-codes.const";
+import { HttpStatus } from "@nestjs/common";
 describe("UserService", () => {
   let userService: UsersService;
-  let mockPrisma: jest.Mocked<PrismaService>;
+  let mockPrisma: {
+    user: {
+      findUnique: jest.Mock;
+      update: jest.Mock;
+      delete: jest.Mock;
+    };
+  };
   let mockFilesService: jest.Mocked<FilesService>;
   let mockQueueService: jest.Mocked<PhotoQueueService>;
 
   const mockUser = {
-    id: "123",
+    id: "user-123",
     name: "user",
     email: "kirill@gmail.com",
     password: "hash_password",
@@ -31,37 +40,158 @@ describe("UserService", () => {
 
   beforeEach(async () => {
     jest.clearAllMocks();
+
+    const prismaMock = {
+      user: {
+        findUnique: jest.fn(),
+        update: jest.fn(),
+        delete: jest.fn(),
+      },
+    };
+
+    const photoQueueMock = {
+      sendToPhotoEditor: jest.fn(),
+    };
+
+    const filesServiceMock = {
+      uploadFile: jest.fn(),
+      deleteAllUserFiles: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
         {
           provide: PrismaService,
-          useValue: {
-            user: {
-              findUnique: jest.fn(),
-              update: jest.fn(),
-              delete: jest.fn(),
-            },
-          },
+          useValue: prismaMock,
         },
         {
           provide: PhotoQueueService,
-          useValue: {
-            sendToPhotoEditor: jest.fn(),
-          },
+          useValue: photoQueueMock,
         },
         {
           provide: FilesService,
-          useValue: {
-            uploadFile: jest.fn(),
-            deleteAllUserFiles: jest.fn(),
-          },
+          useValue: filesServiceMock,
         },
       ],
     }).compile();
-    userService = module.get(UsersService);
-    mockPrisma = module.get(PrismaService) as jest.Mocked<PrismaService>;
-    mockFilesService = module.get(FilesService) as jest.Mocked<FilesService>;
-    mockQueueService = module.get(PhotoQueueService) as jest.Mocked<PhotoQueueService>;
+
+    userService = module.get<UsersService>(UsersService);
+    mockPrisma = module.get(PrismaService) as any;
+    mockFilesService = module.get(FilesService) as any;
+    mockQueueService = module.get(PhotoQueueService) as any;
+  });
+
+  describe("updateUser", () => {
+    const userId = "user-123";
+
+    it("should update user name", async () => {
+      const dto: UpdateUserDto = {
+        name: "newUser",
+      };
+
+      const updatedUserMock = {
+        ...mockUser,
+        name: dto.name,
+      };
+
+      mockPrisma.user.update.mockResolvedValue(updatedUserMock);
+      const result = await userService.updateUser(userId, dto);
+
+      expect(mockPrisma.user.update).toHaveBeenCalledWith({
+        where: { id: userId },
+        data: dto,
+      });
+      expect(result).toEqual(toDto(updatedUserMock, ReadUserDto));
+    });
+    it("should update user mail", async () => {
+      const dto: UpdateUserDto = {
+        email: "NewEmail@mail.com",
+      };
+
+      const updatedUserMock = {
+        ...mockUser,
+        email: dto.email,
+      };
+
+      mockPrisma.user.update.mockResolvedValue(updatedUserMock);
+      const result = await userService.updateUser(userId, dto);
+
+      expect(result).toEqual(toDto(updatedUserMock, ReadUserDto));
+    });
+
+    it("should call hash function", async () => {
+      const dto: UpdateUserDto = {
+        password: "User123456.",
+      };
+      const hashedPassword = "hashed_password_123";
+      const updatedUserMock = {
+        ...mockUser,
+        password: hashedPassword,
+      };
+
+      (argon2.hash as jest.Mock).mockResolvedValue(hashedPassword);
+
+      mockPrisma.user.update.mockResolvedValue(updatedUserMock);
+      const result = await userService.updateUser(userId, dto);
+
+      expect(argon2.hash).toHaveBeenCalledWith(dto.password);
+
+      expect(argon2.hash).toHaveBeenCalledTimes(1);
+    });
+    it("should update user all", async () => {
+      const dto: UpdateUserDto = {
+        password: "User123456.",
+        name: "newUser",
+        email: "newUser@mail.ru",
+      };
+      const hashedPassword = "hashed_password_123";
+      const updatedUserMock = {
+        ...mockUser,
+        password: hashedPassword,
+        name: dto.name,
+        email: dto.email,
+      };
+
+      (argon2.hash as jest.Mock).mockResolvedValue(hashedPassword);
+
+      mockPrisma.user.update.mockResolvedValue(updatedUserMock);
+      const result = await userService.updateUser(userId, dto);
+
+      expect(argon2.hash).toHaveBeenCalledWith(dto.password);
+      expect(argon2.hash).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(toDto(updatedUserMock, ReadUserDto));
+    });
+    it("should not update anything when dto is empty", async () => {
+      const dto: UpdateUserDto = {};
+      mockPrisma.user.update.mockResolvedValue(mockUser);
+      const result = await userService.updateUser(userId, dto);
+
+      expect(result).toEqual(toDto(mockUser, ReadUserDto));
+    });
+  });
+  describe("deleteUser", () => {
+    const userId = "user-123";
+
+    it("should throw error if user not found", async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(null);
+
+      await expect(userService.deleteUser(userId)).rejects.toThrow(ApiException);
+
+      await expect(userService.deleteUser(userId)).rejects.toMatchObject({
+        errorCode: ErrorCodes.USER_NOT_FOUND,
+      });
+    });
+
+    it("should call delete all user files", async () => {
+      mockPrisma.user.findUnique.mockResolvedValue(mockUser);
+      mockFilesService.deleteAllUserFiles.mockResolvedValue({ deletedCount: 5 });
+      await userService.deleteUser(userId);
+      expect(mockFilesService.deleteAllUserFiles).toHaveBeenCalledWith(userId);
+
+      expect(mockPrisma.user.delete).toHaveBeenCalledWith({
+        where: { id: userId },
+      });
+    });
   });
 });
