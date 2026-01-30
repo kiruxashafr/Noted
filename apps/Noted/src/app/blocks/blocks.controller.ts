1453badd3ca32296996f0a187e0d2ee56f782566
kiruxashafr <kirshafranov@gmail.com>
2026-01-30 21:56:38 +0300

    feat: block controller

--- apps/Noted/src/app/blocks/blocks.controller.ts
+++ apps/Noted/src/app/blocks/blocks.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { BlocksService } from "./blocks.service";
+import { CreateBlockDto } from "./dto/create-block.dto";
+import { JwtAuthGuard } from "../auth/guards/jwt.guards";
+import { Request } from "express";
 
 @Controller("blocks")
 export class BlocksController {
-  constructor(private readonly blocksServise: BlocksService) {}
+  constructor(private readonly blocksServise: BlocksService) { }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Req() req: Request,
+    @Body() dto: CreateBlockDto) {
+    return this.blocksServise.createBlock(req.user.sub, dto);
+  }
+
 }
